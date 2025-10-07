efa9d532a31ceb5f1dc63aa6392e25152e9a3818
Raul-data <rauljusto7@outlook.com>
2025-10-07 14:23:43 +0200

    new

--- app/src/main/java/com/example/proyecto_aplicacion_medidas_244/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_aplicacion_medidas_244/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.proyecto_aplicacion_medidas_244
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.jvm.java
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,6 +18,13 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        //Añadimos el listener para el boton "Tomar medida"
+        val botonTomarMedidas = findViewById<Button>(R.id.boton_tomar_medidas)
+        botonTomarMedidas.setOnClickListener {
+            val intent = Intent(this, TomarMedidasActivity::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/com/example/proyecto_aplicacion_medidas_244/TomarMedidasActivity.kt
@@ -0,0 +1,12 @@
+package com.example.proyecto_aplicacion_medidas_244
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class TomarMedidasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tomar_medidas)
+
+    }
+}
